203c4b46e8ecbf3e7883714777602689f9eec286
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2024-04-02 23:03:44 +0100

    added meals page and its components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealGrid from "../components/meals/meal-grid";
+
 export default function MealsPage() {
-    return <h1>Meals page</h1>;
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meal created{" "}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite recipes and cook it by yourself.</p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share">Share your favorite recipe</Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <MealGrid meals={[]} />
+            </main>
+        </>
+    );
 }
